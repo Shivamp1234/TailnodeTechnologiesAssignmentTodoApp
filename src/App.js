b234296d4a4f36d2f4567942125fa7506996b217
Shivamp1234 <patilshivam156@gmail.com>
2023-09-28 10:34:14 +0530

    Added localstorage functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './index.css'
 import Form from './Components/Form'
 import TodoList from './Components/TodoList'
 
+const getLocalTodos = () => {
+  let localTodos = localStorage.getItem("todos")
+  if (localTodos) {
+    return JSON.parse(localStorage.getItem("todos"))
+  }
+  else {
+    return []
+  }
+}
+
 const App = () => {
   const [inputText, setInputText] = useState("")
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(getLocalTodos())
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
+
   return (
     <>
       <header>
